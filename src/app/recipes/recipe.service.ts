28a4e9aa240fef852ac11d0de5ee48b0a3b91f70
Bryan Jackson <bjack13@gmail.com>
2017-05-08 12:51:06 -0600

    adding recipe service to replace EventEmitter use.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core'
+
 import { Recipe } from './recipe.model'
 
 export class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Test','this is a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
     ,new Recipe('Another Test','this is a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
