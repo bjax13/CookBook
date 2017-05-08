1a307cc6a2a144538eaa6ba67a881be84f92df6a
Bryan Jackson <bjack13@gmail.com>
2017-05-08 13:59:59 -0600

    added full ingredient list to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,16 +12,29 @@ export class RecipeService{
       "The secret to this recipe is the addition of canned fire-roasted tomatoes, which add lots of flavor with hardly any effort. We bolster the dish's savory side with Spanish chorizo and smoked paprika and add in chicken and canned chickpeas for a dinner that's as hearty as it is easy",
       'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
       [
-        new Ingredient('meat', 1),
-        new Ingredient('cheese', 1),
+        new Ingredient('Chorizo', 8),
+        new Ingredient('Parsley Leaves', 1),
+        new Ingredient('Sherry Vinegar', 1),
+        new Ingredient('Salt', 1),
+        new Ingredient('Pepper', 1),
+        new Ingredient('Chicken', 1),
+        new Ingredient('Chicken Stock', 1),
+        new Ingredient('Onion', 1),
+        new Ingredient('Spanish Paprika', 1),
+        new Ingredient('Chickpeas', 2),
+        new Ingredient('Olive Oil', 1),
       ])
     ,new Recipe(
       'Salmon Roasted in Butter',
       "This simple fish dish is best made with wild salmon, but it works equally well with the farmed sort. It's astonishingly easy. In a hot oven, melt butter in a skillet until it sizzles, add the salmon, flip, remove the skin, then allow to roast a few minutes more. You'll have an elegant fish dinner in about 15 minutes. Don't be afraid to play with herb and fat combinations: parsley, chervil or dill work well with butter; thyme, basil or marjoram with olive oil; or peanut oil with cilantro or mint.",
       'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg',
       [
-        new Ingredient('fish', 1),
-        new Ingredient('butter', 1),
+        new Ingredient('Salmon Fillet', 1),
+        new Ingredient('Dill', 1),
+        new Ingredient('Butter', 1),
+        new Ingredient('Salt', 1),
+        new Ingredient('Pepper', 1),
+        new Ingredient('Lemon', 1),
       ])
   ];
 
