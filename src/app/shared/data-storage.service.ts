daea712d03b2ebbaa89526e0d6eaa2ecb45b422f
Bryan Jackson <bjack13@gmail.com>
2017-05-12 10:44:19 -0600

    fetch recipes from server

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@angular/core'
-import { Http } from '@angular/http'
+import { Injectable } from '@angular/core';
+import { Http , Response} from '@angular/http';
+import 'rxjs/add/operator/map'
 
-import { RecipeService } from '../recipes/recipe.service'
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -9,5 +11,26 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://udemycookbook.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes(){
+    this.http.get('https://udemycookbook.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if(!recipe['ingredients']){
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
